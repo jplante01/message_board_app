70723d5543faf2d3244f1ae70877fb22772fcfdc
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-08-17 12:58:13 -0700

    Create initial user model mockup

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const mongoDB =
+  'mongodb+srv://jefe:lh32UULXq0Wzp3nK@cluster0.heqtz5w.mongodb.net/?retryWrites=true&w=majority';
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 30 },
+  last_name: { type: String, required: true, maxLength: 30 },
+  username: { type: String, required: true, maxLength: 30 },
+  password: { type: String, required: true },
+  membership_status: { type: Boolean, default: false }
+});
+
+UserSchema.virtual('name').get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = '';
+  if (this.first_name && this.last_name) {
+    fullname = `${this.last_name}, ${this.first_name}`;
+  }
+
+  return fullname;
+});
+
+// Create a static method to get a user's messages?
+
+module.exports = mongoose.model("User", UserSchema);
