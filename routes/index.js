131fe381302c952f13839195f2cdc5a75cda31d8
jplante01 <90069927+jplante01@users.noreply.github.com>
2023-12-26 11:02:58 -0800

    Create route and page for admin dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,20 @@ router.get('/', function(req, res, next) {
   res.redirect('/messages');
 });
 
+router.get('/admin', requireRole('admin'), function(req, res, next) {
+  res.render('home', {
+    user: req.user,
+    pageContent: 'dashboard',
+  });
+});
+
+function requireRole(role) {
+  return (req, res, next) => {
+    if (req.isAuthenticated() && req.user[role] === true) {
+      return next();
+    }
+    res.status(403).json({ message: 'Forbidden' });
+  };
+}
 
 module.exports = router;
